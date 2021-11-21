023a61e666dbe0364537514d29f319484ea9c61c
Junghyo Yang <juyang@c6r4s5.42seoul.kr>
2021-11-21 18:52:29 +0900

    [Feat] Save After Train Theta

--- estimate_two.py
+++ estimate_two.py
@@ -14,9 +14,10 @@ def normalize(data):
 	return tmp_data
 
 def denormalize(data_normal, data):
-	data_normal[:, 0] = data_normal[:, 0] * np.std(data[:, 0]) + np.mean(data[:, 0])
-	data_normal[:, 1] = data_normal[:, 1] * np.std(data[:, 1]) + np.mean(data[:, 1])
-	return data_normal
+	tmp_data = data_normal.copy()
+	tmp_data[:, 0] = tmp_data[:, 0] * np.std(data[:, 0]) + np.mean(data[:, 0])
+	tmp_data[:, 1] = tmp_data[:, 1] * np.std(data[:, 1]) + np.mean(data[:, 1])
+	return tmp_data
 
 def plot_data(data, x, y):
 	plt.plot(data[:, 0], data[:, 1], 'o')
@@ -26,10 +27,11 @@ def plot_data(data, x, y):
 	plt.show()
 
 def estimate_price(data, theta):
-	data[:, 1] = theta[0] + theta[1] * data[:, 0]
-	return data
+	tmp_data = data.copy()
+	tmp_data[:, 1] = theta[0] + theta[1] * tmp_data[:, 0]
+	return tmp_data
 
-def train(data, theta, learning_rate, iteration):
+def train(data, theta, learning_rate, iteration, origin):
 	for ith in range(iteration):
 		temp_theta = theta[len(theta) - 1]
 		weight_arr = []
@@ -37,21 +39,28 @@ def train(data, theta, learning_rate, iteration):
 		for item in data:
 			bias_arr.append(estimate(item[0], temp_theta) - item[1])
 			weight_arr.append((estimate(item[0], temp_theta) - item[1]) * item[0])
+		# print(data)
+		# print(bias_arr)
+		# print(weight_arr)
+		print('bias mean : ' + str(np.mean(bias_arr)) + ' weight mean : ' + str(np.mean(weight_arr)))
+		# plot_data(origin, denormalize(estimate_price(data, temp_theta), origin)[:, 0], denormalize(estimate_price(data, temp_theta), origin)[:, 1])
 		temp_theta[0] = temp_theta[0] - (learning_rate * np.mean(bias_arr))
 		temp_theta[1] = temp_theta[1] - (learning_rate * np.mean(weight_arr))
 		theta = np.concatenate((theta, temp_theta.reshape((1, -1))), axis = 0)
 		print('theta0 : ' + str(temp_theta[0]) + ' theta1 : ' + str(temp_theta[1]))
+	df = pd.DataFrame(theta)
+	df.to_csv("theta_after_train.csv")
 	return theta
 
 def	main():
-	learning_rate = 0.5
-	iteration = 1000
+	learning_rate = 0.05
+	iteration = 100
 
 	theta = getTheta()
 	data = getData()
 	data_normalize = normalize(data)
 
-	train_theta = train(data_normalize, theta, learning_rate, iteration)
+	train_theta = train(data_normalize, theta, learning_rate, iteration, data)
 
 	result = estimate_price(data_normalize, train_theta[len(train_theta) - 1])
 	data_denormalize = denormalize(result, data)
